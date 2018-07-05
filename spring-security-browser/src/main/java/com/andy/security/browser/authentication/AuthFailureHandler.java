6a3ef8cbd5b0ff930738264108ce7c87446b9c77
lyon <rethinkt@163.com>
2018-07-05 22:52:02 +0800

    update some file

--- spring-security-browser/src/main/java/com/andy/security/browser/authentication/AuthFailureHandler.java
+++ spring-security-browser/src/main/java/com/andy/security/browser/authentication/AuthFailureHandler.java
@@ -1,24 +1,20 @@
 package com.andy.security.browser.authentication;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.andy.security.browser.support.SimpleResponse;
+import com.andy.security.core.properties.LoginType;
+import com.andy.security.core.properties.SecurityProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.andy.security.core.properties.LoginType;
-import com.andy.security.core.properties.SecurityProperties;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author ruolin create by 2017年11月19日下午3:12:07
@@ -27,25 +23,25 @@ import com.andy.security.core.properties.SecurityProperties;
 @Component("authFailureHandler")
 public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Autowired
-	private SecurityProperties securityProperties;
+    @Autowired
+    private SecurityProperties securityProperties;
 
-	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException exception) throws IOException, ServletException {
-		log.info("登录失败！");
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        log.info("登录失败！");
 //		response.setContentType("application/json;charset=utf-8");
 //		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 //		response.getWriter().write(objectMapper.writeValueAsString(exception));
-		if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-			response.setContentType("application/json;charset=utf-8");
-			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-			response.getWriter().write(objectMapper.writeValueAsString(new SimpleResponse(exception.getMessage())));
-		} else {
-			super.onAuthenticationFailure(request, response, exception);
-		}
-	}
+        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+            response.setContentType("application/json;charset=utf-8");
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.getWriter().write(objectMapper.writeValueAsString(new SimpleResponse(exception.getMessage())));
+        } else {
+            super.onAuthenticationFailure(request, response, exception);
+        }
+    }
 }
--- spring-security-browser/src/main/java/com/andy/security/browser/authentication/AuthSuccessHandler.java
+++ spring-security-browser/src/main/java/com/andy/security/browser/authentication/AuthSuccessHandler.java
@@ -25,25 +25,25 @@ import com.andy.security.core.properties.SecurityProperties;
 @Component("authSuccessHandler")
 public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Autowired
-	private SecurityProperties securityProperties;
+    @Autowired
+    private SecurityProperties securityProperties;
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse resp, Authentication authentication)
-			throws IOException, ServletException {
-		log.info("登录成功！");
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse resp, Authentication authentication)
+            throws IOException, ServletException {
+        log.info("登录成功！");
 //		resp.setContentType("application/json;charset=utf-8");
 //		resp.getWriter().write(objectMapper.writeValueAsString(authentication));
-		if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-			resp.setContentType("application/json;charset=utf-8");
-			resp.getWriter().write(objectMapper.writeValueAsString(authentication));
-		} else {
-			super.onAuthenticationSuccess(req, resp, authentication);
-		}
+        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+            resp.setContentType("application/json;charset=utf-8");
+            resp.getWriter().write(objectMapper.writeValueAsString(authentication));
+        } else {
+            super.onAuthenticationSuccess(req, resp, authentication);
+        }
 
-	}
+    }
 
 }
