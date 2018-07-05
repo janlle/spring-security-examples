1d91857dcd76cc358d7a8cf9c41c72f00919f04b
lyon <rethinkt@163.com>
2018-07-05 22:49:07 +0800

    update some file

--- spring-security-auth-server/src/main/java/com/andy/auth/OAuthApplication.java
+++ spring-security-auth-server/src/main/java/com/andy/auth/OAuthApplication.java
@@ -6,8 +6,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 /**
  * 认证服务器
- * @author ruolin
- * create by 2018年5月12日上午8:36:39
+ *
+ * @author Mr.lyon
+ * @create by 2018年5月12日上午8:36:39
  */
 @SpringBootApplication
 @EnableAuthorizationServer
