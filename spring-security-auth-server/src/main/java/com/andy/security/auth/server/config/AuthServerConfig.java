9e059e87a4d7b85aa5b27e4f2d7076ca89d74240
leone <exklin@gmail.com>
2018-11-01 09:23:22 +0800

    fix(all):update auth server

---
+++ spring-security-auth-server/src/main/java/com/andy/security/auth/server/config/AuthServerConfig.java
@@ -0,0 +1,14 @@
+package com.andy.security.auth.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @author Leone
+ * @since 2018-05-23
+ **/
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig {
+
+}
